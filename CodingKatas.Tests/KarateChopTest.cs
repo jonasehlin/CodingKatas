feb6f657d5ddde5713e4dd5d8f63c6eb681ca258
Jonas Ehlin <jonas.ehlin@joliv.se>
2017-11-29 20:13:02 +0100

    Added more test cases.

--- CodingKatas.Tests/KarateChopTest.cs
+++ CodingKatas.Tests/KarateChopTest.cs
@@ -41,6 +41,20 @@ namespace CodingKatas.Tests
 			Assert.AreEqual(-1, karate.Chop(4, new int[] { 1, 3, 5, 7 }));
 			Assert.AreEqual(-1, karate.Chop(6, new int[] { 1, 3, 5, 7 }));
 			Assert.AreEqual(-1, karate.Chop(8, new int[] { 1, 3, 5, 7 }));
+
+			Assert.AreEqual(0, karate.Chop(1, new int[] { 1, 2, 3, 5, 7, 11, 13, 17, 19 }));
+			Assert.AreEqual(1, karate.Chop(2, new int[] { 1, 2, 3, 5, 7, 11, 13, 17, 19 }));
+			Assert.AreEqual(2, karate.Chop(3, new int[] { 1, 2, 3, 5, 7, 11, 13, 17, 19 }));
+			Assert.AreEqual(3, karate.Chop(5, new int[] { 1, 2, 3, 5, 7, 11, 13, 17, 19 }));
+			Assert.AreEqual(4, karate.Chop(7, new int[] { 1, 2, 3, 5, 7, 11, 13, 17, 19 }));
+			Assert.AreEqual(5, karate.Chop(11, new int[] { 1, 2, 3, 5, 7, 11, 13, 17, 19 }));
+			Assert.AreEqual(6, karate.Chop(13, new int[] { 1, 2, 3, 5, 7, 11, 13, 17, 19 }));
+			Assert.AreEqual(7, karate.Chop(17, new int[] { 1, 2, 3, 5, 7, 11, 13, 17, 19 }));
+			Assert.AreEqual(8, karate.Chop(19, new int[] { 1, 2, 3, 5, 7, 11, 13, 17, 19 }));
+
+			Assert.AreEqual(-1, karate.Chop(-1, new int[] { 1, 2, 3, 5, 7, 11, 13, 17, 19 }));
+			Assert.AreEqual(-1, karate.Chop(15, new int[] { 1, 2, 3, 5, 7, 11, 13, 17, 19 }));
+			Assert.AreEqual(-1, karate.Chop(30, new int[] { 1, 2, 3, 5, 7, 11, 13, 17, 19 }));
 		}
 	}
 }
