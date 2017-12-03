d8e4c0d67bb5a47aed68d2cd8a75ff8a19084fb9
Jonas Ehlin <jonas.ehlin@joliv.se>
2017-12-03 16:20:48 +0100

    Added data file reader classes.

---
+++ DataMunging/DataFile.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DataMunging
+{
+	public class DataFile
+	{
+		public List<string> Columns => new List<string>();
+		public List<DataRow> Rows => new List<DataRow>();
+
+		public void LoadFromString(string data)
+		{
+			Load(new StringReader(data));
+		}
+
+		public void Load(TextReader reader)
+		{
+			while (reader.Peek() >= 0)
+			{
+				ReadColumns(reader.ReadLine());
+			}
+		}
+
+		private void ReadColumns(string line)
+		{
+			Columns.Clear();
+			while (ReadText(line, out string column))
+			{
+				Columns.Add(column);
+				line = line.Substring(column.Length);
+			}
+		}
+
+		private bool ReadText(string line, out string column)
+		{
+			column = null;
+
+			if (line == null)
+				return false;
+
+			var sb = new StringBuilder();
+			bool leadingWhitespace = true;
+			foreach (var ch in line)
+			{
+				if (char.IsWhiteSpace(ch))
+				{
+					if (!leadingWhitespace)
+						break;
+				}
+				else
+				{
+					sb.Append(ch);
+					leadingWhitespace = false;
+				}
+			}
+
+			if (sb.Length > 0)
+				column = sb.ToString();
+
+			return column != null;
+		}
+	}
+}
